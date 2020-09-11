06f7d7ba087255c7fb40c80a6d56cc74034d3ac6
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2020-09-11 11:08:25 +0800

    启动脚本

---
+++ start_asgi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+echo "以asgi形式启动SMA后端服务"
+
+echo "查询已运行的asgi服务"
+
+ps -ef | grep 'uvicorn'
+
+read -r -p "请输入asgi的pid > " pid
+
+kill "$pid"
+
+nohup uvicorn am_sma.asgi:application --host 0.0.0.0 --port 5000 > asgi.log &
+jobs
