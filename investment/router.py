86bcc84c43cd16a2cd79f56bac03bb74292b0053
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2020-12-15 17:34:52 +0800

    添加备用数据库，实现数据库读写分离

--- investment/router.py
+++ investment/router.py
@@ -2,8 +2,10 @@
 class Router(object):
     @staticmethod
     def db_for_read(model, **hints):
+        """读取使用备库"""
         return 'slave'
 
     @staticmethod
     def db_for_write(model, **hints):
+        """写入使用主库"""
         return 'default'
