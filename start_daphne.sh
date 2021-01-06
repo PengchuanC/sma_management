da917ac76063eba68033db2e7f763ab57f46a8ea
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-01-06 15:15:48 +0800

    添加定时任务

--- start_daphne.sh
+++ start_daphne.sh
@@ -2,4 +2,4 @@
 echo start backend server
 
 nohup daphne -p 5000 sma_management.asgi:application > daphne.log &
-nohup python3 manage.py qcluster > django_q.log &
+nohup /usr/local/python3/bin/python3 manage.py qcluster > django_q.log &
