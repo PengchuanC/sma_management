1f4d0e12c5098eb6bcb72c76a1eea0cbf1999d1e
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-01-06 16:40:28 +0800

    添加新的任务

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,12 @@
 from tasks.prev_valuation import pre_valuation
+from command import run_all
 
 
 def save_prev_valuation_nav():
     """保存组合预估净值到数据库"""
     pre_valuation()
+
+
+def commit_all_db_task():
+    """执行所有同步任务"""
+    run_all()
