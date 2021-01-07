1b10b96469aad9f59af231ce630c5d231c14d2d0
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-01-07 15:16:35 +0800

    添加客户申赎申请

--- start_daphne.sh
+++ start_daphne.sh
@@ -1,6 +1,8 @@
 
 echo start backend server
 
-ps aux|grep daphne|grep -v grep|cut -c 9-15|xargs kill -9 && nohup daphne -p 5000 sma_management.asgi:application > daphne.log &
+ps aux|grep daphne|grep -v grep|cut -c 9-15|xargs kill -9
+ps aux|grep qcluster|grep -v grep|cut -c 9-15|xargs kill -9
 
-ps aux|grep qcluster|grep -v grep|cut -c 9-15|xargs kill -9 && nohup /usr/local/python3/bin/python3 manage.py qcluster > django_q.log &
+nohup daphne -p 5000 sma_management.asgi:application > daphne.log &
+nohup /usr/local/python3/bin/python3 manage.py qcluster > django_q.log &
