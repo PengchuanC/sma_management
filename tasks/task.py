cb142cdcde2b9013817a5567c1aa41997012b28e
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-01-07 09:17:50 +0800

    添加客户申赎记录表

--- tasks/task.py
+++ tasks/task.py
@@ -1,5 +1,5 @@
 from tasks.prev_valuation import pre_valuation
-from command import run_all
+from command import shu_commit, fund_limit, tools, fund_fee
 
 
 def save_prev_valuation_nav():
@@ -9,4 +9,11 @@ def save_prev_valuation_nav():
 
 def commit_all_db_task():
     """执行所有同步任务"""
-    run_all()
+    shu_commit()
+    tools.commit_stocks()
+    tools.commit_fund()
+    tools.commit_index_gil()
+    tools.commit_tradingdays()
+    tools.commit_preprocess()
+    fund_limit.commit_fund_limit()
+    fund_fee.commit_fund_fee()
