2fe3d17fa3f102ac782e6ba79190b33a62638232
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-01-13 15:33:52 +0800

    修正爬虫执行问题和股票行业同步问题

--- start_daphne.sh
+++ start_daphne.sh
@@ -4,5 +4,7 @@ echo start backend server
 ps aux|grep daphne|grep -v grep|cut -c 9-15|xargs kill -9
 ps aux|grep qcluster|grep -v grep|cut -c 9-15|xargs kill -9
 
+sleep 3s
+
 nohup daphne -p 5000 sma_management.asgi:application > daphne.log &
 nohup /usr/local/python3/bin/python3 manage.py qcluster > django_q.log &
