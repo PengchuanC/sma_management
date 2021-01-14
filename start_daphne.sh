cfe66d02e19984c0164d51d89d40a77b8ccc335c
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-01-14 11:05:44 +0800

    由于主从操作时常崩溃，放弃主从分离

--- start_daphne.sh
+++ start_daphne.sh
@@ -3,8 +3,10 @@ echo start backend server
 
 ps aux|grep daphne|grep -v grep|cut -c 9-15|xargs kill -9
 ps aux|grep qcluster|grep -v grep|cut -c 9-15|xargs kill -9
+ps aux|grep stock_async|grep -v grep|cut -c 9-15|xargs kill -9
 
 sleep 3s
 
 nohup daphne -p 5000 sma_management.asgi:application > daphne.log &
 nohup /usr/local/python3/bin/python3 manage.py qcluster > django_q.log &
+nohup /usr/local/python3/bin/python3 -m crwal.stock_async.py
