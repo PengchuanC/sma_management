e2b0a81f22a6e712a6e555b79e4d7bb25b2b52df
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-03-03 17:19:59 +0800

    添加换手率

--- investment/views/history.py
+++ investment/views/history.py
@@ -2,11 +2,14 @@
 history
 ~~~~~~~
 查看调仓历史和计算历史调仓费用
+
+@modify: 2021-03-03
+@desc: 加入换手率
 """
 
 import datetime
 
-from django.db.models import Sum, Max
+from django.db.models import Sum, Max, Min, Avg
 from rest_framework.views import APIView, Response
 
 from investment.models.portfolio import Transactions as ta
@@ -15,8 +18,8 @@ from investment.utils import fund
 
 
 class HistoryView(APIView):
-    @staticmethod
-    def get(request):
+
+    def get(self, request):
         today = datetime.date.today().strftime('%Y-%m-%d')
         port_code = request.query_params.get('portCode')
         date = request.query_params.get('date')
@@ -31,10 +34,13 @@ class HistoryView(APIView):
             port_code=port_code, date__lte=date, operation__icontains='成交确认'
         ).aggregate(date=Max('date'))['date']
         exact = ta.objects.filter(port_code=port_code, date=last).aggregate(total=Sum('fee'))['total']
+
+        turn_over = self.turn_over(port_code)
         return Response({
             'total': {'fee': total, 'ratio': total / net_asset},
             'last': {'fee': exact, 'ratio': exact / net_asset},
-            'date': last
+            'date': last,
+            'turn_over': turn_over
         })
 
     @staticmethod
@@ -71,6 +77,29 @@ class HistoryView(APIView):
         name_filter = [{'text': x, 'value': x} for x in sorted(set(name_filter))]
         date_filter = [{'text': x, 'value': x} for x in sorted(set(date_filter))]
         operation_filter = [{'text': x, 'value': x} for x in operation_filter]
+
         return Response(
             {'data': ret, 'filter': {'operation': operation_filter, 'name': name_filter, 'date': date_filter}}
         )
+
+    @staticmethod
+    def turn_over(port_code):
+        """计算换手率"""
+        finish = ['开放式基金申购成交确认', '开放式基金赎回成交确认', '开放式基金转换转出成交确认', '开放式基金转换转入成交确认']
+        records = ta.objects.filter(port_code=port_code, operation__in=finish).order_by('date').all()
+        cap = 0
+        for r in records:
+            if r.operation == '开放式基金申购成交确认':
+                cap += - r.operation_amount
+            elif r.operation == '开放式基金赎回成交确认':
+                cap += r.operation_amount
+            elif r.operation == '开放式基金转换转出成交确认':
+                cap += r.order_value * r.order_price
+            else:
+                cap += r.order_value * r.order_price + r.fee
+        # 获取期初和期末平均资产
+        dates = bl.objects.filter(port_code=port_code).aggregate(min=Min('date'), max=Max('date'))
+        dates = [dates['min'], dates['max']]
+        asset = bl.objects.filter(port_code=port_code, date__in=dates).aggregate(avg=Avg('net_asset'))['avg']
+        turn_over = cap / asset
+        return turn_over
