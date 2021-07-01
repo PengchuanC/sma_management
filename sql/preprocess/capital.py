6706b271db4b5d37bf3fb6cc7fa811bef0e19db9
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-07-01 14:29:29 +0800

    fix(资金流向): 修复资金流向同步两次的问题
    资金流向会出现两次同步数据

--- sql/preprocess/capital.py
+++ sql/preprocess/capital.py
@@ -24,6 +24,7 @@ def commit_capital_flow():
     data = pd.DataFrame(data)
     data = data[data.agg(lambda x: x.date > latest.get(x.secucode, datetime.date(2021, 1, 1)), axis=1)]
     data.columns = [x.lower() for x in data.columns]
+    data = data.drop_duplicates(['secucode', 'date'])
     m = [models.IndustryCF(**x) for _, x in data.iterrows()]
     models.IndustryCF.objects.bulk_create(m)
 
