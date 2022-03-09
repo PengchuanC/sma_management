394510a360faeebf3a7b8f2bc7839e3fc523d0ce
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2022-03-09 10:47:09 +0800

    refactor(冗余代码): 删除确定已废弃功能的冗余代码

--- tasks/task.py
+++ tasks/task.py
@@ -1,15 +1,12 @@
 from tasks.prev_valuation import pre_valuation
 from tasks.asset_allocate import portfolio_asset_allocate
 from tasks import fund_position
-from shu.from_sma import commit_sma
 from sql import tools
 from crawl.stock_async import executor
 from crawl.fund_fee_howbuy import commit_fund_fee_hb
 from crawl.fund_fee import commit_fund_fee_em
 from sql.commit.funds_extend import commit_fund_quote
-from services.client import Client
 from services.backtest_client import Client as BacktestClient
-from sma_management.settings import RpcProxyHost
 
 
 def save_prev_valuation_nav():
@@ -17,25 +14,6 @@ def save_prev_valuation_nav():
     tools.commit_stock_daily_quote()
     commit_fund_quote()
     pre_valuation()
-
-
-def commit_all_db_task():
-    """同步组合净值"""
-    with Client(RpcProxyHost) as client:
-        try:
-            client.commit_all()
-        except Exception as e:
-            print(e)
-
-
-def commit_all_db_task_hook(task):
-    commit_sma()
-    tools.commit_return_yield()
-
-
-def crawl_stock_price():
-    """异步爬取股票实时价格"""
-    executor()
 
 
 def commit_capital():
